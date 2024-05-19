689d9835c4853c93c46f22b42524c1350aead268
unknown <Hoang Hai Phong>
2024-05-19 22:15:28 +0700

    update source code to add todo list

--- source.js
+++ source.js
@@ -1,1 +1,14 @@
 "use strict";
+const input = document.getElementById("inputbox");
+const container = document.getElementById("container");
+function addTask(){
+    if(input === ""){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        container.appendChild(li);
+    }
+    input.value = "";
+}
