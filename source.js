2b0de0f7f96d8e654021c1b3bf4ef57fd68f9e5c
unknown <Hoang Hai Phong>
2024-05-20 20:01:19 +0700

    add saved data json file, update new source js code to save data

--- source.js
+++ source.js
@@ -1,14 +1,25 @@
 "use strict";
-const input = document.getElementById("inputbox");
+
+let fs = require('fs');
+
+const newTask = document.getElementById("inputbox");
 const container = document.getElementById("container");
+
+
 function addTask(){
-    if(input === ""){
+    if(newTask === ""){
         alert("You must write something");
     }
     else{
+        fs.writeFile("/data.json", newTask.value, function(err){
+            if(err){
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
         let li = document.createElement("li");
-        li.innerHTML = input.value;
+        li.innerHTML = newTask.value;
         container.appendChild(li);
     }
-    input.value = "";
+    newTask.value = "";
 }
