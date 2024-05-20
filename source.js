19d0be958081df148c3015846425f47e6cff9eee
unknown <Hoang Hai Phong>
2024-05-20 20:33:11 +0700

    last commit, my code finally worked ToT

--- source.js
+++ source.js
@@ -2,24 +2,30 @@
 
 let fs = require('fs');
 
-const newTask = document.getElementById("inputbox");
-const container = document.getElementById("container");
+let readline = require('readline');
 
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 
-function addTask(){
-    if(newTask === ""){
-        alert("You must write something");
+});
+let newTask = {
+    task: undefined
+};
+rl.on("line", (task) =>{
+    newTask.task = task;
+}).on("close", ()=>{
+    const myData = JSON.stringify(newTask);
+    if(newTask.task === ""){
+        console.log("You must write something");
     }
     else{
-        fs.writeFile("/data.json", newTask.value, function(err){
+        fs.writeFile("/Javascript/Todolist app/data.json", myData, function(err){
             if(err){
                 return console.log(err);
             }
             console.log("The file was saved!");
         });
-        let li = document.createElement("li");
-        li.innerHTML = newTask.value;
-        container.appendChild(li);
     }
-    newTask.value = "";
-}
+});
+// console.log()
